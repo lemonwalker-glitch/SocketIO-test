97a25d90a5b948512cdf5e7c176be35030c4310d
lemonwalker-glitch <lemonwalker7@gmail.com>
2019-09-02 03:37:45 +0400

    4th commit

--- chatapp.py
+++ chatapp.py
@@ -22,7 +22,13 @@ def handle_my_custom_event(json):
 @socketio.on('my redirect')# this one listens until in the jquery an emit() is sent to my redirect and then this function sends a emit function back calling the redirect in the html
 def handle_my_custom_event(json):
     print(str(json))
+    #socketio.emit('redirect', {'url': url_for('welcome')})
+    redirect()
+
+
+def redirect():
     socketio.emit('redirect', {'url': url_for('welcome')})
+    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
